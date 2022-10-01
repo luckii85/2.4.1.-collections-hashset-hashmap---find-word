a65d709917a678dcfc560f23ee69e74fedc162d8
luckii85 <luckii@ya.ru>
2022-10-02 03:04:45 +0400

    alternative function with HashSet

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,17 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class WordsChecker {
 
     private String text;
-    List<String> words = new ArrayList<>();
 
     public WordsChecker(String text) {
         this.text = text;
     }
 
+    List<String> words = new ArrayList<>();
+
     public boolean hasWord(String word) {
         words = List.of(text.split("\\P{IsAlphabetic}+"));
         return words.contains(word);
+    }
+
+    Set<String> words2 = new HashSet<>();
+
+    public boolean hasWord2(String word) {
+String[] parts = text.toLowerCase().split("\\P{IsAlphabetic}+");
+// Collections.addAll(words2, parts);
+// words2.addAll(List.of(parts));
+// words2.addAll(Arrays.asList(parts));
+        for (String part : parts){
+            words2.add(part);
+        }
+return words2.contains(word.toLowerCase());
     }
 }
